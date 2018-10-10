80c7942139509bc7ae1234abe3f8f11b815d2bdf
mr_wj <1477563131@qq.com>
2018-10-10 17:45:29 +0800

    消费者

---
+++ consumer-demo/src/main/java/cn/itcast/consumer/ConsumerDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerDemoApplication.class, args);
+    }
+}
