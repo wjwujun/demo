f53fb4843ff8f24658335dfab90ca0f85147e53f
mr_wj <1477563131@qq.com>
2018-10-16 11:49:19 +0800

    eureka-client

--- user-service/src/main/java/cn/itcast/userservice/UserServiceApplication.java
+++ user-service/src/main/java/cn/itcast/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package cn.itcast.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cn.itcast.userservice.mapper")
 public class UserServiceApplication {
