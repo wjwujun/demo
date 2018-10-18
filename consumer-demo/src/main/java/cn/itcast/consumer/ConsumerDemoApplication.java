82acd357ff7f01acc3914539f61e0d94076a529c
mr_wj <1477563131@qq.com>
2018-10-18 17:13:45 +0800

    ribbon负载和重试机制

--- consumer-demo/src/main/java/cn/itcast/consumer/ConsumerDemoApplication.java
+++ consumer-demo/src/main/java/cn/itcast/consumer/ConsumerDemoApplication.java
@@ -3,6 +3,7 @@ package cn.itcast.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class ConsumerDemoApplication {
 
     /*客户端请求方法*/
     @Bean
+    @LoadBalanced           //负载均衡
     public RestTemplate restTemplate(){
         return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
--- consumer-demo/src/main/java/cn/itcast/consumer/dao/UserDao.java
+++ consumer-demo/src/main/java/cn/itcast/consumer/dao/UserDao.java
@@ -2,12 +2,9 @@ package cn.itcast.consumer.dao;
 
 import cn.itcast.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Component
 public class UserDao {
@@ -20,12 +17,17 @@ public class UserDao {
 
     public User queryGetById(Long id){
         //获取服务
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        //List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
         //如果是多个,需要编写负载均衡算法
-        ServiceInstance instance = instances.get(0);
+        //ServiceInstance instance = instances.get(0);
 
-         String url="http://"+instance.getHost()+":"+instance.getPort()+"/user/"+id;
+        //String url="http://"+instance.getHost()+":"+instance.getPort()+"/user/"+id;
+
+        String url="http://user-service/user/"+id;      //负载均衡
         return this.restTemplate.getForObject(url,User.class);
+
+
+
     }
 
 }
