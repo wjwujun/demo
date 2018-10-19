8056596de0d32cec4b058a869758dfecb8769e84
mr_wj <1477563131@qq.com>
2018-10-19 16:58:02 +0800

    hystrix超时熔断测试，睡眠时间

--- user-service/src/main/java/cn/itcast/userservice/service/UserService.java
+++ user-service/src/main/java/cn/itcast/userservice/service/UserService.java
@@ -6,7 +6,7 @@ import cn.itcast.userservice.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.Random;
 
 
 @Service
@@ -16,6 +16,14 @@ public class UserService {
     private UserMapper userMapper;
 
     public User queryById(Long id){
+
+        //睡眠,用于测试hystrix的超时熔断功能
+        try {
+            Thread.sleep(new Random().nextInt(2000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return this.userMapper.selectByPrimaryKey(id);
     }
 
