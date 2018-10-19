ebd522ee9c5c77d6f617c7514a08df753e4989f0
mr_wj <1477563131@qq.com>
2018-10-19 17:55:37 +0800

    zuul网关

---
+++ zuul-demo/src/main/java/cn/itcast/zuuldemo/ZuulDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.zuuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulDemoApplication.class, args);
+    }
+}
